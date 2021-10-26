a07692855f2ac2cd1a5a40a619181856fca64c55
Karmester <ptrarmand10@gmail.com>
2021-10-26 15:42:06 +0200

    Change nav

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 function buttonClicked(event) {
-	//console.log(event.target.classList);
-	//console.log(event.target);
-	//event.target.classList.toggle("clicked");
-	const randomText = "Ezt vajon kilogolja-e?";
-	const fastLogResult = fastLog("Ezt a szöveget szeretném kiloggoltatni");
-	fastLog(randomText);
-	console.log(fastLogResult);
-	document.getElementById("about").classList.add("clicked-too");
+	event.target.classList.toggle("clicked");
+	document.getElementById("nav").classList.toggle("clicked");
 }
 
 function loadEvent() {
-	//console.log("the page has loaded");
+	console.log("the page has loaded");
+
 	document.getElementById("menu-btn").addEventListener("click", buttonClicked);
 }
-function fastLog(text) {
-	console.log(text);
-	return "text logged muhaha";
-}
-
 window.addEventListener("load", loadEvent);
